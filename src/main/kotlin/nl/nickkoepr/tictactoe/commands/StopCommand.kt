e4e435213488f655b6f145fd98380f1a0dfcadb8
Nick <65495045+NickKoepr@users.noreply.github.com>
2022-01-04 22:23:09 +0100

    Removed an unnecessary check in StopCommand.kt

--- src/main/kotlin/nl/nickkoepr/tictactoe/commands/StopCommand.kt
+++ src/main/kotlin/nl/nickkoepr/tictactoe/commands/StopCommand.kt
@@ -25,64 +25,62 @@ class StopCommand(override val description: String, override val name: String) :
         val handler = BotUtil.getUnknownMessageHandler(event.message)
         val channel = event.channel
 
-        if (BotUtil.hasAllPermissions(event.message)) {
-            if (GameRequestManager.hasSendRequest(userId)) {
-                GameRequestManager.cancelRequest(userId)
+        if (GameRequestManager.hasSendRequest(userId)) {
+            GameRequestManager.cancelRequest(userId)
+            channel.sendMessage(
+                MessageUtil.successMessage(
+                    "Your game request has been canceled",
+                    "Your game request has been canceled successfully."
+                )
+            ).queue(null, handler)
+        } else if (GameManager.hasGame(userId)) {
+            val game =
+                if (GameManager.checkPlayersTurn(userId)) GameManager.getGame(userId)!! else GameManager.getGameFromUser(
+                    userId
+                )!!
+            if (!game.finished) {
+                GameManager.gameStopRequest(
+                    game,
+                    userId
+                )
                 channel.sendMessage(
                     MessageUtil.successMessage(
-                        "Your game request has been canceled",
-                        "Your game request has been canceled successfully."
+                        "Your game is cancelled",
+                        "Your cancelled your game successfully."
                     )
                 ).queue(null, handler)
-            } else if (GameManager.hasGame(userId)) {
-                val game =
-                    if (GameManager.checkPlayersTurn(userId)) GameManager.getGame(userId)!! else GameManager.getGameFromUser(
-                        userId
-                    )!!
-                if (!game.finished) {
-                    GameManager.gameStopRequest(
-                        game,
-                        userId
-                    )
+            } else {
+                if (!GameManager.hasPlayerChosenRematch(userId)) {
+                    BotUtil.jda.getTextChannelById(game.channelId)?.retrieveMessageById(game.message)?.queue({
+                        GameManager.playerRematchChoice(
+                            Player(user.name, Position.X, user.id),
+                            game,
+                            false,
+                            it
+                        )
+                    }, handler)
                     channel.sendMessage(
                         MessageUtil.successMessage(
-                            "Your game is cancelled",
-                            "Your cancelled your game succesfully."
+                            "Your game request has been declined",
+                            "Your game request has been declined successfully."
                         )
                     ).queue(null, handler)
                 } else {
-                    if (!GameManager.hasPlayerChosenRematch(userId)) {
-                        BotUtil.jda.getTextChannelById(game.channelId)?.retrieveMessageById(game.message)?.queue({
-                            GameManager.playerRematchChoice(
-                                Player(user.name, Position.X, user.id),
-                                game,
-                                false,
-                                it
-                            )
-                        }, handler)
-                        channel.sendMessage(
-                            MessageUtil.successMessage(
-                                "Your game request has been declined",
-                                "Your game request has been declined successfully."
-                            )
-                        ).queue(null, handler)
-                    } else {
-                        channel.sendMessage(
-                            MessageUtil.errorMessage(
-                                "You already accepted the rematch!",
-                                "You cannot cancel the rematch because you already accepted it."
-                            )
-                        ).queue(null, handler)
-                    }
+                    channel.sendMessage(
+                        MessageUtil.errorMessage(
+                            "You already accepted the rematch!",
+                            "You cannot cancel the rematch because you already accepted it."
+                        )
+                    ).queue(null, handler)
                 }
-            } else {
-                channel.sendMessage(
-                    MessageUtil.errorMessage(
-                        "You are not playing a game or have sent a request!",
-                        "You can only use the stop command when you are playing a game and if you have sent a request.",
-                    )
-                ).queue(null, handler)
             }
+        } else {
+            channel.sendMessage(
+                MessageUtil.errorMessage(
+                    "You are not playing a game or have sent a request!",
+                    "You can only use the stop command when you are playing a game and if you have sent a request.",
+                )
+            ).queue(null, handler)
         }
     }
 }
